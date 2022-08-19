ec51bf2f0d39c825e45d7a97f9399e843d4635ce
Kevin Souto <kevinsoutods@gmail.com>
2022-08-18 22:20:46 -0300

    Bloqueando acesso de usuarios não logados

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { onAuthStateChanged } from 'firebase/auth';
 
-//hooks
+// hooks
 import { useState, useEffect } from 'react';
 import { useAuthentication } from './hooks/useAuthentication';
 
-//context
+// context
 import { AuthProvider } from "./context/AuthContext";
 
-
+// components
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
+// pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import CreatePost from './pages/CreatePost/CreatePost'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
   
@@ -47,8 +50,10 @@ function App() {
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
+              <Route path='/register' element={!user ? <Register /> : <Navigate to="/"/>} />
+              <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to="/login"/>} />
+              <Route path='/dashboard' element={user ? <Dashboard/> : <Navigate to="/login"/>} />
             </Routes>
           </div>
         <Footer />
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,14 @@
 import styles from './About.module.css'
 import React from 'react'
 
+import {Link} from "react-router-dom";
+
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h2>Sobre o Mini <span>Blog</span></h2>
+        <p>Este projeto consiste em um blog feito com React no FrontEnd e Firebase no BackEnd.</p>
+        <Link to="/posts/create" className="btn">Criar Post</Link>
     </div>
   )
 }
