7a950f0170ab71c13fe0344c12d194cd150cd3bf
Kevin Souto <kevinsoutods@gmail.com>
2022-08-23 19:44:16 -0300

    Criando página de post individual

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -9,7 +9,7 @@ const PostDetails = ({ post }) => {
     <div className={styles.post_details}>
         <img src={post.image} alt={post.title} />
         <h2>{post.title}</h2>
-        <p className={styles.createdBy}>{post.createdBy}</p>
+        <p className={styles.createdBy}>Criado por: {post.createdBy}</p>
         <div className={styles.tags}>
             {post.tagsArray.map((tag) => (
                 <p key={tag}>
---
+++ src/pages/Post/Post.jsx
@@ -0,0 +1,19 @@
+import styles from "./Post.module.css"
+
+// hooks
+import { useParams } from "react-router-dom"
+
+import React from 'react'
+
+const Post = () => {
+
+  const { id } = useParams();  
+
+  return (
+    <div>
+        <h1>Post {id}</h1>
+    </div>
+  )
+}
+
+export default Post
