dcc8091cd61b3dacd5f4fc58363f662fa094818c
Kevin Souto <kevinsoutods@gmail.com>
2022-08-23 18:05:17 -0300

    Exibindo resultados da busca

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -27,7 +27,7 @@ export const useFetchDocuments = ( docCollection, search = null, uid = null ) =>
                 // dashboard
 
                 if (search) {
-                    q = await query(collectionRef, where("tags", "array-contains", search), orderBy("createdAt", "desc"))
+                    q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"))
                 } else {
                     q = await query(collectionRef, orderBy("createdAt", "desc"));
                 }
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import styles from "./Search.module.css"
 
+// hooks
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import { useQuery } from '../../hooks/useQuery'
+
+// components
+import PostDetails from '../../components/PostDetails';
+
+import {Link} from "react-router-dom"
 
 const Search = () => {
 
   const query = useQuery();
-  const search  = query.get("q");  
+  const search  = query.get("q"); 
+  
+  const {documents: posts} = useFetchDocuments("posts", search);
 
   return (
-    <div>
+    <div className={styles.search_container}>
         <h1>Search Page</h1>
-        <p>{search}</p>
+        <div>
+            {posts && posts.length === 0 && (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts a partir da sua busca</p>
+                    <Link to="/" className="btn btn-dark">Voltar</Link>
+                </div>
+            )}
+
+            {posts && posts.map((post) => (
+                <PostDetails key={post.id} post={post}/>
+            ))}
+        </div>
     </div>
 
   )
