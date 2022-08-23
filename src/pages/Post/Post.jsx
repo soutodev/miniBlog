56a9797fbfb1316c92a49a3919b21370d64010ec
Kevin Souto <kevinsoutods@gmail.com>
2022-08-23 20:20:50 -0300

    Estilizando página de post individual

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -9,18 +9,27 @@ import React from 'react'
 const Post = () => {
 
   const { id } = useParams();  
-  const {document:post, loading} = useFetchDocument("posts", id);
+  const { document:post, loading } = useFetchDocument("posts", id);
 
   return (
-    <div>
+    <div className={styles.post_container}>
 
         {loading && <p>Carregando...</p>}
 
         {post && (
             <>
                 <h1>{post.title}</h1>
+                <img src={post.image} alt={post.title} />
+                <p>{post.body}</p>
+                <h3>Este post trata sobre:</h3>
+                <div className={styles.tags}>
+                    {post.tagsArray.map((tag) => (
+                        <p key={tag}><span>#</span>{tag}</p>
+                    ))}
+                </div>
             </>
         )}
+
     </div>
   )
 }
