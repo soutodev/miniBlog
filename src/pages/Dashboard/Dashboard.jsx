edc923cb18a816f38950704cfe4b4d5d7e684030
Kevin Souto <kevinsoutods@gmail.com>
2022-08-26 00:00:17 -0300

    Função de edição completa, faltam testes

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom';
 // hooks
 import {useAuthValue} from "../../context/AuthContext";
 import {useFetchDocuments} from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from '../../hooks/useDeleteDocument';
 
 
 const Dashboard = () => {
@@ -16,9 +17,8 @@ const Dashboard = () => {
   // posts do usuário
   const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
 
-  const deleteDocument = (id) => {
+  const {deleteDocument} = useDeleteDocument("posts");
 
-  }
 
   if(loading) {
     return <p>Carregando...</p>
